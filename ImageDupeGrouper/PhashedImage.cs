b09c70dbde7c20c4ee3e52c0862ae2b8203d5739
Jason Carr <jason@unbrokensoftware.com>
2020-06-11 13:49:51 -0700

    Initial commit

---
+++ ImageDupeGrouper/PhashedImage.cs
@@ -0,0 +1,21 @@
+﻿namespace ImageDupeGrouper
+{
+    using System.Drawing;
+
+    using Shipwreck.Phash;
+    using Shipwreck.Phash.Bitmaps;
+
+    public class PhashedImage
+    {
+        public PhashedImage(string filePath)
+        {
+            this.FilePath = filePath;
+            using var bitmap = (Bitmap)Image.FromFile(filePath);
+            this.Phash = ImagePhash.ComputeDigest(bitmap.ToLuminanceImage());
+        }
+
+        public string FilePath { get; }
+
+        public Digest Phash { get; }
+    }
+}
---
+++ ImageDupeGrouper/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace ImageDupeGrouper
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Threading.Tasks;
+
+    using Shipwreck.Phash;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            float requiredScore = float.Parse(args[0]);
+            var files = new ConcurrentBag<string>();
+
+            for (int i = 1; i < args.Length; i++)
+            {
+                files.Add(args[i]);
+            }
+
+            var images = new ConcurrentBag<PhashedImage>();
+
+            Parallel.ForEach(
+                files,
+                filePath =>
+                {
+                    images.Add(new PhashedImage(filePath));
+                });
+
+            var groups = new ConcurrentBag<ConcurrentBag<PhashedImage>>();
+
+            foreach (var image in images)
+            {
+                bool found = false;
+
+                foreach (var group in groups)
+                {
+                    foreach (var image2 in group)
+                    {
+                        float score = ImagePhash.GetCrossCorrelation(image.Phash, image2.Phash);
+
+                        if (score < requiredScore)
+                        {
+                            continue;
+                        }
+
+                        group.Add(image);
+                        found = true;
+                        break;
+                    }
+
+                    if (found)
+                    {
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    groups.Add(new ConcurrentBag<PhashedImage>
+                    {
+                        image
+                    });
+                }
+            }
+
+            foreach (var group in groups)
+            {
+                foreach (var image in group)
+                {
+                    Console.WriteLine(image.FilePath);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
